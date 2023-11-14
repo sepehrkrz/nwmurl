bea7bd654734314ab092898fdddf35b0d3b8da9c
sepehrkrz <sepehrkz@yahoo.com>
2023-11-14 18:47:55 +0000

    Added zar base url for retrospective data

--- .ipynb_checkpoints/setup-checkpoint.py
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -33,7 +33,7 @@ Then, you can use it in your Python code:
 """
 
 # Other information
-VERSION = "0.1.7"
+VERSION = "0.1.8"
 DESCRIPTION = "URL generator tool for National Water Model data"
 
 setup(
